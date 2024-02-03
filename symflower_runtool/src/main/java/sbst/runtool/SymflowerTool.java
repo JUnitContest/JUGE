21efd6922d41bea80257da3c11403fbbf7ca068c
soleritu <uzayavci@gmail.com>
2024-02-03 02:29:30 +0100

    Added Symflower skeleton, removed instancio

---
+++ symflower_runtool/src/main/java/sbst/runtool/SymflowerTool.java
@@ -0,0 +1,76 @@
+package sbst.runtool;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.io.IOException;
+
+public class SymflowerTool implements ITestingTool {
+
+    /**
+     * The class under test classpath
+     */
+    private String cutClassPath;
+
+    /**
+     * List of additional class path entries required by a testing tool
+     *
+     * @return List of directories/jar files
+     */
+    @Override
+    public List<File> getExtraClassPath() {
+        return null;
+    }
+
+    /**
+     * Initialize the testing tool, with details about the code to be tested (SUT)
+     * Called only once.
+     *
+     * @param src       Directory containing source files of the SUT
+     * @param bin       Directory containing class files of the SUT
+     * @param classPath List of directories/jar files (dependencies of the SUT)
+     */
+    @Override
+    public void initialize(File src, File bin, List<File> classPath) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(bin.getAbsolutePath());
+        for (File f : classPath) {
+            sb.append(File.pathSeparator);
+            sb.append(f.getAbsolutePath());
+        }
+        this.cutClassPath = sb.toString();
+    }
+
+    /**
+     * Run the test tool, and let it generate test cases for a given class
+     *
+     * @param cName      Name of the class for which unit tests should be generated
+     * @param timeBudget How long the tool must run to test the class (in
+     *                   miliseconds)
+     */
+    @Override
+    public void run(String cName, long timeBudget) {
+        List<String> commands = new ArrayList<>();
+        commands.add("lib/symflower");
+        commands.add("unit-tests");
+        // commands.add("--workspace=" + this.cutClassPath); // Set the root directory
+        // of the project to analyze
+        // commands.add("--java-test-framework=automatic"); // Set the Java test
+        // framework
+        // commands.add("--output=json"); // Set the output format to JSON
+        commands.add("--tests-path=temp/testcases"); // Set the path where test files are placed into
+        // commands.add("--test-generation-timeout=" + timeBudget); // Set the test
+        // generation timeout
+        commands.add("--class=" + cName); // Set the class name to analyze
+        // commands.add("--memory-limit=1024"); // Set the memory limit
+
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        try {
+            Process process = processBuilder.start();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
